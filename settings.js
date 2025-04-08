7ce1e11740eb74a5ed4ce74b07d7c367efa387d1
alstonhsiao <42710829+alstonhsiao@users.noreply.github.com>
2025-04-08 23:32:47 +0800

    Update settings.js
    add user name alstonh and pw st

--- settings.js
+++ settings.js
@@ -3,5 +3,16 @@ module.exports = {
   httpAdminRoot: '/admin',         // 管理介面路徑
   httpNodeRoot: '/',              // 節點 API 路徑
   userDir: './data',              // 資料儲存目錄
-  functionGlobalContext: {}       // 全局上下文
+  functionGlobalContext: {},      // 全局上下文
+  // 啟用身份驗證
+  adminAuth: {
+    type: "credentials",
+    users: [
+      {
+        username: "alstonh", // 用戶名
+        password: "$2a$08$oqQr0hTArRNRqkojvp/FoOzIPdlM9J/ds.XNIJinXUt6i821agyhy", // 加密後的密碼
+        permissions: "*"   // 完全權限
+      }
+    ]
+  }
 };
